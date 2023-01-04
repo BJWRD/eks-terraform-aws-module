1a35459c2d2ac1b1afd5fcabbe3554ff7903743d
Brad Whitcomb <83971386+BJWRD@users.noreply.github.com>
2023-01-04 15:36:43 +0000

    VPC Config Update

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,3 +1,47 @@
 ################################################################################
 # VPC - variables.tf
 ################################################################################
+
+################################################################################
+# VPC - variables.tf
+################################################################################
+
+variable "project_name" {
+    description = "Name of Project"
+}
+
+variable "environment" {
+    description = "Environment type"
+}
+
+variable "source" {
+    description = "Source Code"
+}
+
+variable "region" {
+  description = "This is the cloud hosting region where your webapp will be deployed."
+}
+
+variable "vpc_name" {
+    description = "Name of VPC"
+}
+
+variable "vpc_cidr" {
+    description = "CIDR Range"
+}
+
+variable "azs" {
+    description = "Availability Zones"
+}
+
+variable "private_subnets" {
+    description = "Private Subnet Range"
+}
+
+variable "public_subnets" {
+    description = "Public Subnet Range"
+}
+
+variable "destination_cidr_block" {
+    description = "The CIDR Block Desination"
+}
